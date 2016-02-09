eb0ea410a6ab736aa498f0dd473bfafe3d80913c
Sean S. King <seanscottking@gmail.com>
2016-02-09 17:57:48 -0500

    Adding vagrant facts.d fact

--- site/profile/manifests/base/vagrant.pp
+++ site/profile/manifests/base/vagrant.pp
@@ -2,5 +2,15 @@
 #
 #
 class profile::base::vagrant {
-  
+
+  file { '/etc/facter/facts.d/vagrant.txt':
+    ensure  => present,
+    require => [ Mkdir::P['/etc/facter/facts.d'], ],
+  } ->
+  file_line { 'vagrant_fact':
+    line  => 'vagrant=vagrant',
+    path  => '/etc/facter/facts.d/vagrant.txt',
+    match => '^vagrant=',
+  }
+
 }
