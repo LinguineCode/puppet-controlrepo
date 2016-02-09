5f887efd6fda1217965015a66c42ee112be19f2e
Sean S. King <seanscottking@gmail.com>
2016-02-09 00:43:08 -0500

    MOTD wasn't working for Vagrant

--- site/profile/manifests/base/motd.pp
+++ site/profile/manifests/base/motd.pp
@@ -2,45 +2,45 @@
 #
 #
 class profile::base::motd {
+  
+  package { 'figlet': }
 
-  if $::osfamily == 'RedHat' {
+  if $::is_pe == true {
+    $agent = 'Puppet Enterprise Agent'
+    $puppet_service = 'pe-puppet'
+    $classestxt = '/var/opt/lib/pe-puppet/classes.txt'
+  }
+  else {
+    $agent = 'Puppet Agent'
+    $puppet_service = 'puppet'
+    $classestxt = '/var/lib/puppet/state/classes.txt'
+  }
 
-    package { 'figlet': }
-    
-    $ascii = generate('/bin/sh', '-c', "/usr/bin/figlet -c -w 60 ${::domain}")
-
-    if $::is_pe == true {
-      $agent      = 'Puppet Enterprise Agent'
-      $classestxt = '/var/opt/lib/pe-puppet/classes.txt'
-    }
-    else {
-      $agent      = 'Puppet Agent'
-      $classestxt = '/var/lib/puppet/classes.txt'
-    }
-
-    if !$app_role { $app_role = 'none' }
-    if !$app_tier { $app_tier = 'none' }
-
-    file { '/etc/profile.d/motd.sh':
-      mode    => '0755',
-      content => template('profile/motd/motd.sh.erb')
-    }
-
-    file { '/etc/issue.net':
-      mode    => '0644',
-      content => template('profile/motd/issue.net.erb')
-    }
-
+  if !$::vagrant {
     file { '/tmp/classes.tmp':
       source             => $classestxt,
       source_permissions => 'ignore',
     }
-
-
   }
 
-  else {
-    notify { "${::osfamily} OS family not supported": }
+  if !$app_role { $app_role = 'none' }
+  if !$app_tier { $app_tier = 'none' }
+  
+  case $::osfamily {
+    'RedHat': {
+      file { '/etc/profile.d/motd.sh':
+        mode    => '0755',
+        content => template('profile/motd/motd.sh.erb'),
+        require => [ Package['figlet'], ],
+      }
+      file { '/etc/issue.net':
+        mode    => '0644',
+        content => template('profile/motd/issue.net.erb'),
+        notify  => [ Service['sshd'], ],
+      }
+    }
+
+    default: { notify { "${::osfamily} OS family not supported": } }
   }
 
 }
