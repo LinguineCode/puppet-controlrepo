0579a818b5af3d25fba9698401067454e98182cf
Sean S. King <seanscottking@gmail.com>
2016-02-23 20:50:19 -0500

    More Debian compatibility

--- Vagrant-puppet/install_puppet.sh
+++ Vagrant-puppet/install_puppet.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 e() {
   echo "$0: $@"
 }
@@ -13,33 +12,44 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-if [ "$(which puppet)" ]; then
-  # Puppet already installed, exiting silently
-  exit 0
-fi
+if [ -f '/etc/redhat-release' ]; then
 
-if [ -f /etc/redhat-release ]; then
+  if [ "$(which puppet)" ]; then
+    # Puppet already installed, exiting silently
+    exit 0
+  else
 
-  yum install -y redhat-lsb-core
+    yum install -y redhat-lsb-core
 
-  case "$(lsb_release -sr | cut -d'.' -f1)" in
-
-    6)
-      yum install -y http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-      yum install -y https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-      ;;
-
-    *)
-      err "Unsupported Operating System Version"
-      exit 1
-      ;;
-
-  esac
+    case "$(lsb_release -sr | cut -d'.' -f1)" in
+  
+        6)
+          yum install -y http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+          yum install -y https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+          ;;
+          
+        *)
+          err "Unsupported Operating System Version"
+          exit 1
+          ;;
+  
+    esac
+  fi
   
   yum install -y puppet
 
-#elif [ -f /etc/debian_version ]; then
-#  # do some Debian stuff here
+elif [ -f '/etc/debian_version' ]; then
+  
+  if [ "$(dpkg -S puppetlabs-release 2>/dev/null)" ]; then
+    # Puppet repo already installed, exiting silently
+    exit 0
+  else
+    SHORT_CODENAME="$(lsb_release -c -s)"
+    wget -O /tmp/puppetlabs-release-"$SHORT_CODENAME".deb https://apt.puppetlabs.com/puppetlabs-release-"$SHORT_CODENAME".deb
+    dpkg -i /tmp/puppetlabs-release-"$SHORT_CODENAME".deb
+    apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confnew" install puppet
+  fi
 
 else
   err "Unsupported Operating System"
