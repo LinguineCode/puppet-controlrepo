d7dd17b69ea79ae66e0d90de98843d905de8e712
Sean S. King <seanscottking@gmail.com>
2016-04-10 19:44:19 -0400

    Add hiera level for aws

---
+++ site/profile/manifests/base/aws.pp
@@ -0,0 +1,14 @@
+# Class: aws
+#
+#
+class profile::base::aws {
+
+  $aws_admin_username = downcase($::operatingsystem)
+  
+  # FIXME: Figure out how to do this in Hiera
+  profile::base::access_and_sudoers::allow { $aws_admin_username:
+    sudoer_content => 'ALL=(ALL) NOPASSWD:ALL',
+  }
+    
+
+}
